656140a2e48eca4203861f9e467c7542d79822cf
Munjeong Kim <doorjeong@gmail.com>
2018-02-01 18:38:54 +0900

    #7 기존 web chatting에 jQuery Mobile 적용

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
-소켓통신 (web chatting)
+소켓통신 (web chatting + jQuery Mobile)
 ================
 
-웹에서 채팅하는 프로그램
+웹에서 채팅하는 프로그램 + jQuery Mobile
 
 # 설치 방법
 'npm install' 실행해주세요.
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var io = socketio.listen(server); // 소켓서버 생성 및 실행
 io.sockets.on('connection', function(socket){
     console.log('connection 이벤트 발생 !!!');
     socket.on('sendMsg', function(data){
-        console.log('sendMsg 이벤트 발생 :: ' + data);
+        console.log('sendMsg 이벤트 발생 :: ', data);
         io.sockets.emit('outputMsg', data); // 클라이언트쪽에서 input text 입력받은 값
     });
 });
