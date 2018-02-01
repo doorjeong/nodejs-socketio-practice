9981f3ce9393c9ae0457650a8b1854a1436f7891
Munjeong Kim <doorjeong@gmail.com>
2018-02-01 17:12:26 +0900

    #6 web chatting 구현

--- index.js
+++ index.js
@@ -2,33 +2,21 @@ var http = require('http');
 var fs = require('fs');
 var socketio = require('socket.io');
 
-var server = http.createServer(); // 웹서버 생성
-var io = socketio.listen(server); // 소켓서버 생성 및 실행
-
-// 웹서버 실행
-server.listen(3000, function(){
-    console.log('Server Running');
-});
-
-// request 이벤트 발생시
-server.on('request', function(request, response){
+var server = http.createServer(function(request, response){
     fs.readFile('HTMLPage.html', function(error, data){
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end(data);
     });
+}).listen(3000, function(){
+    console.log('Server Running');
 });
 
-var roomName = null;
+var io = socketio.listen(server); // 소켓서버 생성 및 실행
+
 io.sockets.on('connection', function(socket){
-
-    socket.on('join', function(data){
-        roomName = data; // 클라이언트쪽에서 prompt로 사용자한테 입력받은 방이름
-        socket.join(data);
-        console.log("join된 방이름 : " + roomName);
-    });
-
+    console.log('connection 이벤트 발생 !!!');
     socket.on('sendMsg', function(data){
-        console.log('방이름:' + roomName + ' / 메시지: ' + data);
-        io.sockets.in(roomName).emit('outputMsg', data); // 클라이언트쪽에서 input text 입력받은 값
+        console.log('sendMsg 이벤트 발생 :: ' + data);
+        io.sockets.emit('outputMsg', data); // 클라이언트쪽에서 input text 입력받은 값
     });
 });
